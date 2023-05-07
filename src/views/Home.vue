096fa42259667630a25c0c0410626855c7bf9568
zokoko <zokoko@163.com>
2023-05-07 14:51:04 +0800

    增加算法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App" time="5000" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" time="5000" /> -->
+    <div>fast</div>
   </div>
 </template>
 
@@ -14,7 +15,7 @@ export default {
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
       temptxt: 'hello'
     }
@@ -24,13 +25,61 @@ export default {
       return this.$store._state.data.module1.count
     }
   },
-  created () {
+  created() {
     var that = this
     console.log('temptxt', that.count)
+    this.algorithmJs()
   },
-  mounted () {},
+  mounted() {},
   methods: {
-    test () {}
+    test() {},
+    algorithmJs() {
+      console.log('algorithmJs')
+
+      // this.longestCommonSubsequence('kabc', 'dddxxkabzzz')
+      this.longestPalindrome("babad")
+    },
+
+    //最长回文子串
+    longestPalindrome(s) {
+      const n = s.length
+      let longest = ''
+      const dp = Array(n)
+        .fill()
+        .map(() => Array(n).fill(false))
+        // console.log(dp);
+
+      for (let i = n - 1; i >= 0; i--) {
+        for (let j = i; j < n; j++) {
+          dp[i][j] = s[i] === s[j] && (j - i < 2 || dp[i + 1][j - 1])
+          if (dp[i][j] && j - i + 1 > longest.length) {
+            longest = s.substring(i, j + 1)
+          }
+        }
+      }
+
+      return longest
+    },
+    //最长公共子序列
+    longestCommonSubsequence(str1, str2) {
+      const m = str1.length
+      const n = str2.length
+      const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0))
+      // console.log('dp',dp,new Array(m + 1).fill(0));
+      for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+          if (str1[i - 1] === str2[j - 1]) {
+            dp[i][j] = dp[i - 1][j - 1] + 1
+            console.log('eq', i, j, str1[i - 1], str2[j - 1], dp[i][j], dp)
+          } else {
+            dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j])
+          }
+        }
+      }
+      return dp[m][n]
+    },
+
+    
   }
 }
 </script>
